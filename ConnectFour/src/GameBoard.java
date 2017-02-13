cef65040ce3628adda7444009596dbe8213dfacb
petruzlj <petruzlj@miamioh.edu>
2017-02-13 17:02:24 -0500

    AI is better kinda, program tracks wins/ties, auto-game buttons let you simulate AI vs AI

--- ConnectFour/src/GameBoard.java
+++ ConnectFour/src/GameBoard.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -16,13 +17,17 @@ import javax.swing.JToolBar;
 @SuppressWarnings("serial")
 public class GameBoard extends JFrame implements ActionListener {
 	private JButton[][] buttons = new JButton[7][6];
-	private JButton checkButton = new JButton("Check");
-	private JButton tipButton = new JButton("Tip");
+	private JButton autoOnceButton = new JButton("Auto-Game");
+	private JButton auto10Button = new JButton("Auto-Game x10");
+	private JButton auto100Button = new JButton("Auto-Game x100");
 	private JButton clearButton = new JButton("Clear");
 	private JLabel barLabel = new JLabel();
 	private int[][] iconColor = new int[7][6];
 	private int iconCount = 1;
 	private int winner = 0;
+	private int redWinCount = 0;
+	private int yellowWinCount = 0;
+	private int tieCount = 0;
 	private ImageIcon redCircle = new ImageIcon(getClass().getResource("/resources/Red Circle.png"));
 	private ImageIcon yellowCircle = new ImageIcon(getClass().getResource("/resources/Yellow Circle.png"));
 
@@ -30,6 +35,7 @@ public class GameBoard extends JFrame implements ActionListener {
 		GameBoard cb = new GameBoard();
 		cb.setVisible(true);
 		cb.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 
 	public GameBoard() {
@@ -48,15 +54,18 @@ public class GameBoard extends JFrame implements ActionListener {
 				buttons[i][j] = button;
 			}
 
-		checkButton.addActionListener(this);
-		tipButton.addActionListener(this);
+		autoOnceButton.addActionListener(this);
+		auto10Button.addActionListener(this);
+		auto100Button.addActionListener(this);
 		clearButton.addActionListener(this);
 
 		JToolBar toolbar = new JToolBar();
 		toolbar.setFloatable(false);
-		toolbar.add(checkButton);
+		toolbar.add(autoOnceButton);
 		toolbar.addSeparator();
-		toolbar.add(tipButton);
+		toolbar.add(auto10Button);
+		toolbar.addSeparator();
+		toolbar.add(auto100Button);
 		toolbar.addSeparator();
 		toolbar.add(clearButton);
 		toolbar.addSeparator();
@@ -74,20 +83,29 @@ public class GameBoard extends JFrame implements ActionListener {
 		add(panel);
 		add(toolbar, BorderLayout.PAGE_START);
 		setTitle("8 Queens Problem");
-		barLabel.setText("Red's Turn");
+		barLabel.setText("Red's wins: " + redWinCount + ". Yellow's Wins: " + yellowWinCount + ". Ties: " + tieCount + ".");
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		for (int j = 0; j < buttons[0].length; j++)
 			for (int i = 0; i < buttons.length; i++)
 				if (e.getSource() == buttons[i][j]) {
-					if (buttonPress(i)) {
-						compMove();
-						checkForWin();
-					}
+//					if (buttonPress(i)) {
+						if (!checkForWin(0))
+							compMove();
+						checkForResolution();
+//					}
 				}
 		if (e.getSource() == clearButton) {
 			resetBoard();
+		} else if (e.getSource() == autoOnceButton) {
+			autoGame();
+		} else if (e.getSource() == auto10Button) {
+			for (int i = 0; i < 10; i++)
+				autoGame();
+		} else if (e.getSource() == auto100Button) {
+			for (int i = 0; i < 100; i++)
+				autoGame();
 		}
 	}
 
@@ -108,16 +126,16 @@ public class GameBoard extends JFrame implements ActionListener {
 		}
 
 		if (yIsSafe) {
+			resetBackgrounds();
+			buttons[x][y].setBackground(Color.GREEN);
 			if (iconCount == 1) {
 				iconColor[x][y] = iconCount;
 				buttons[x][y].setIcon(redCircle);
 				iconCount++;
-				barLabel.setText("Yellow's Turn");
 			} else if (iconCount == 2) {
 				iconColor[x][y] = iconCount;
 				buttons[x][y].setIcon(yellowCircle);
 				iconCount--;
-				barLabel.setText("Red's Turn");
 			} else {
 				System.out.println("U DID SOMETHING WRONG");
 			}
@@ -128,15 +146,20 @@ public class GameBoard extends JFrame implements ActionListener {
 
 	public void compMove() {
 		// buttonPress((int)(Math.random()*7));
-		int canWin = -1;
-		int canPreventWin = -1;
-		int canPreventGap = -1;
-		int canPreventAdjacent = -1;
-		int canCenter = -1;
-		int canCorner = -1;
-		int canEdge = -1;
+//		int canWin = -1;
+//		int canPreventWin = -1;
+//		int canPreventGap = -1;
+//		int canPreventAdjacent = -1;
+//		int canCenter = -1;
+//		int canCorner = -1;
+//		int canEdge = -1;
 
 		int firstY = -1;
+		int opponentCount = 0;
+		if (iconCount == 1) opponentCount = 2;
+		else if (iconCount == 2) opponentCount = 1;
+		
+		int[] movePriorities = new int[buttons.length];
 
 		for (int x = 0; x < buttons.length; x++) {
 			boolean yIsSafe = true;
@@ -162,38 +185,57 @@ public class GameBoard extends JFrame implements ActionListener {
 
 				int temp = iconColor[x][y];
 
-				iconColor[x][y] = 2;
+				iconColor[x][y] = iconCount;
 				if (checkForWin(0))
-					if (winner == 2)
-						canWin = x;
+					if (winner == iconCount)
+						movePriorities[x] += 1024;
 
-				iconColor[x][y] = 1;
+				iconColor[x][y] = opponentCount;
 				if (checkForWin(0))
-					if (winner == 1)
-						canPreventWin = x;
+					if (winner == opponentCount)
+						movePriorities[x] += 512;
 
 				iconColor[x][y] = temp;
 
 				if (firstY >= 0) {
-					if (checkForGap(x, firstY, 0))
-						canPreventGap = x + 2;
+					if (checkForGap(x, firstY, 0)) {
+						movePriorities[x] += 128;
+						movePriorities[x+2] += 128;
+						movePriorities[x+4] += 128;
+					}
 
-					if (checkForAdjacent(x, firstY, 0))
-						canPreventAdjacent = x + 1;
+					if (checkForAdjacent(x, firstY, 0)) {
+						movePriorities[x+1] += 128;
+						movePriorities[x+4] += 128;
+					}
 				}
 
 				if (x == buttons.length / 2)
-					canCenter = x;
+					movePriorities[x] += 64;
+				
 				else if (x == 0 || x == buttons.length - 1)
-					canCorner = x;
+					movePriorities[x] += 32;
 				else
-					canEdge = x;
+					movePriorities[x] += 16;
 
 			}
 		}
-
-		System.out.println();
-
+		
+		ArrayList<Integer> indexes = new ArrayList<>();
+		int highest = 0;
+		for (int i = 0; i < movePriorities.length; i++) {
+			if (movePriorities[i] > highest) {
+				indexes.clear();
+				indexes.add(i);
+				highest = movePriorities[i];
+			} else if (movePriorities[i] == highest)
+				indexes.add(i);
+		}
+		
+		int randomChoice = (int) (Math.random()*indexes.size());
+		
+		buttonPress(indexes.get(randomChoice));
+		
 		// System.out.println("canWin == " + canWin);
 		// System.out.println("canPreventWin == " + canPreventWin);
 		// System.out.println("canPreventGap == " + canPreventGap);
@@ -202,26 +244,45 @@ public class GameBoard extends JFrame implements ActionListener {
 		// System.out.println("canCorner == " + canCorner);
 		// System.out.println("canEdge == " + canEdge + "\n");
 
-		if (canWin != -1)
-			buttonPress(canWin);
-		else if (canPreventWin != -1)
-			buttonPress(canPreventWin);
-		else if (canPreventGap != -1)
-			buttonPress(canPreventGap);
-		else if (canPreventAdjacent != -1)
-			buttonPress(canPreventAdjacent);
-		else if (canCenter != -1)
-			buttonPress(canCenter);
-		else if (canCorner != -1)
-			buttonPress(canCorner);
-		else
-			buttonPress(canEdge);
+//		if (canWin != -1)
+//			buttonPress(canWin);
+//		else if (canPreventWin != -1)
+//			buttonPress(canPreventWin);
+//		else if (canPreventGap != -1)
+//			buttonPress(canPreventGap);
+//		else if (canPreventAdjacent != -1)
+//			buttonPress(canPreventAdjacent);
+//		else if (canCenter != -1)
+//			buttonPress(canCenter);
+//		else if (canCorner != -1)
+//			buttonPress(canCorner);
+//		else
+//			buttonPress(canEdge);
+	}
+	
+	public void autoGame() {
+		while(!(checkForTie() || checkForWin(0))) {
+			compMove();
+		}
+		if (checkForWin(0)) {
+			if (winner == 1) {
+				redWinCount++;
+				resetBoard();
+			}
+			if (winner == 2) {
+				yellowWinCount++;
+				resetBoard();
+			}
+		} else if (checkForTie()) {
+			tieCount++;
+			resetBoard();
+		}
+		barLabel.setText("Red's wins: " + redWinCount + ". Yellow's Wins: " + yellowWinCount + ". Ties: " + tieCount + ".");
 	}
 
 	public void resetBoard() {
-		barLabel.setText("Red's Turn");
+		resetBackgrounds();
 		iconCount = 1;
-		tipButton.setEnabled(true);
 		iconColor = new int[buttons.length][buttons[0].length];
 		for (int j = 0; j < buttons[0].length; j++) {
 			for (int i = 0; i < buttons.length; i++) {
@@ -229,20 +290,43 @@ public class GameBoard extends JFrame implements ActionListener {
 			}
 		}
 	}
+	
+	public void resetBackgrounds() {
+		for (int j = 0; j < buttons[0].length; j++) {
+			for (int i = 0; i < buttons.length; i++) {
+				buttons[i][j].setBackground(Color.BLUE);
+			}
+		}
+	}
 
-	public void checkForWin() {
+	public boolean checkForTie() {
+		for (int j = 0; j < iconColor[0].length; j++) {
+			for (int i = 0; i < iconColor.length; i++) {
+				if (iconColor[i][j] == 0)
+					return false;
+			}
+		}
+		return true;
+	}
+	
+	public void checkForResolution() {
 		if (checkForWin(0)) {
 			if (winner == 1) {
-				// barLabel.setText("Red Wins!");
+				redWinCount++;
 				JOptionPane.showMessageDialog(this, "Red Wins!");
 				resetBoard();
 			}
 			if (winner == 2) {
-				// barLabel.setText("Yellow Wins!");
+				yellowWinCount++;
 				JOptionPane.showMessageDialog(this, "Yellow Wins!");
 				resetBoard();
 			}
+		} else if (checkForTie()) {
+			tieCount++;
+			JOptionPane.showMessageDialog(this, "It's a tie because you both suck");
+			resetBoard();
 		}
+		barLabel.setText("Red's wins: " + redWinCount + ". Yellow's Wins: " + yellowWinCount + ". Ties: " + tieCount + ".");
 	}
 
 	public boolean checkForWin(int pos) {
